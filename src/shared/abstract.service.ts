64114e5f6262f8a5a1eacf8a9fa8743db36c0286
João Pacheco <jrpacheco.dev@gmail.com>
2022-01-29 15:28:45 +0000

    abstracted the service

---
+++ src/shared/abstract.service.ts
@@ -0,0 +1,24 @@
+import { Repository } from 'typeorm';
+
+export abstract class AbstractService<T> {
+  protected constructor(protected readonly repository: Repository<any>) {}
+
+  async save(options: Partial<T>) {
+    return await this.repository.save(options);
+  }
+  async findOne(options: Partial<T>) {
+    return await this.repository.findOne(options);
+  }
+
+  async findAll(options: Partial<T>) {
+    return await this.repository.find(options);
+  }
+
+  async update(id: number, options: Partial<T>) {
+    return await this.repository.update(id, options);
+  }
+
+  async delete(id: number) {
+    return await this.repository.delete(id);
+  }
+}
